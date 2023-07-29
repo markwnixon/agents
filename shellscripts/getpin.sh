4a0eb7961ad28e8f8d81a7b199aaffa11b23cb40
mark <markwnixon@gmail.com>
2023-07-29 14:50:35 -0400

    new headless

--- shellscripts/getpin.sh
+++ shellscripts/getpin.sh
@@ -2,6 +2,6 @@
 echo "getpin.sh which runs python code DDD_make_pins.py"
 cd /home/$USER/flask
 source flaskenv/bin/activate
-cd /home/$USER/flask/crontasks
+cd /home/$USER/flask/agents
 python3 FFF_make_pins.py "$1"
 
