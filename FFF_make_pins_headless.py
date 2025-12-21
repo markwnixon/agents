29b85bcb6b455475e251972d7176cd6ca7fa8c38
markwnixon@gmail.com <markwnixon@gmail.com>
2025-12-21 10:27:56 -0500

    test

--- FFF_make_pins_headless.py
+++ FFF_make_pins_headless.py
@@ -20,6 +20,19 @@ from selenium.webdriver.firefox.options import Options
 
 import logging
 
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Headless PIN fetcher")
